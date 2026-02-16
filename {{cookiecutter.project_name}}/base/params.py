65f72bbf6cde0164ad0c220630016a626522a446
james <81617086+je-cook@users.noreply.github.com>
2026-02-16 20:11:34 +0000

    tmp

--- {{cookiecutter.project_name}}/base/params.py
+++ {{cookiecutter.project_name}}/base/params.py
@@ -1,3 +1,5 @@
+{%- set words = cookiecutter.project_name | replace('-', ' ') | replace('_', ' ') | trim | title | replace(' ', '') -%}
+{%- set pascal = words[:1] | upper ~ words[1:] -%}
 # SPDX-FileCopyrightText: {{cookiecutter.copyright_yr}}-present {{ cookiecutter.author_name }} <{{ cookiecutter.author_email }}>
 #
 # SPDX-License-Identifier: MIT
@@ -8,7 +10,7 @@ from dataclasses import dataclass
 from bluemira.base.parameter_frame import Parameter, ParameterFrame
 
 @dataclass
-class {{ cookiecutter.project_name }}Params(ParameterFrame):
+class {{ pascal }}Params(ParameterFrame):
     """{{ cookiecutter.project_name }} Parameters"""
 
     R_0: Parameter[float]
