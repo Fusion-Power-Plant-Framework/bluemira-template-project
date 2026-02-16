37a2f5fb931b43da95880ff92f18c55f8da01c9e
je-cook <81617086+je-cook@users.noreply.github.com>
2026-02-16 16:40:30 +0000

    Move reactor to studies folder (#15)

--- {{cookiecutter.project_name}}/studies/first/run.py
+++
@@ -1,8 +0,0 @@
-"""Study 1."""
-
-from pathlib import Path
-
-from {{cookiecutter.project_name}}.reactor import main
-
-build_config_path = Path(Path(__file__).parent, "config/config.json").resolve()
-reactor = main(build_config_path)
