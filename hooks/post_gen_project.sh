e73831057a5bcdfe49e3c140981bb169c67f980a
james <81617086+je-cook@users.noreply.github.com>
2026-02-16 11:52:45 +0000

    tmp git check

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,6 +3,15 @@
 repo='{{ cookiecutter.project_name }}'
 gh_org='{{ cookiecutter.gh_org_name }}'
 
+
+if git rev-parse --is-inside-work-tree >/dev/null 2>&1 &&
+   [ "$(git rev-list --count HEAD 2>/dev/null)" -gt 1 ]; then
+    echo "Repo with >1 commit"
+else
+    echo "Not a repo or ≤1 commit"
+fi
+
+
 read -p "Do you want to set git repo remote to git@github.com:$gh_org/$repo.git? (y/n)" choice
 case "$choice" in
     y|Y ) echo "Continuing..."; git remote set-url origin git@github.com:$gh_org/$repo.git;
